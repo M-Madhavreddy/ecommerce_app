8d598963a0096f196dea9a2d385d1fa3ca81fcda
M Madhav Reddy <mmrsjce@gmail.com>
2022-11-06 23:07:44 +0530

    ProductOverView

---
+++ lib/Widgets/Productview.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class productview extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  const Card(
+      child: Text('***'),
+    );
+  }
+}
