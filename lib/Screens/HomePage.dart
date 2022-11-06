2572db4b99a656c26607c4c6724064badbf84591
M Madhav Reddy <mmrsjce@gmail.com>
2022-11-06 22:37:29 +0530

    shopApp

---
+++ lib/Screens/HomePage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage_Ecommerce extends StatelessWidget {
+  const HomePage_Ecommerce({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('E-Commerce_App'),
+        ),
+        body: const Center(
+          child: Text('this is just Beginning !!'),
+        ));
+  }
+}
